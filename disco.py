8e16923d53007cbf90e74eef41c848e83250e5e6
Lukas Schaupp <lukasschaupp@gmail.com>
2022-06-05 19:09:58 +0200

    testing

--- disco.py
+++ disco.py
@@ -1944,7 +1944,7 @@ clip_guidance_scale = 5000  # @param{type: 'number'}
 tv_scale = 0  # @param{type: 'number'}
 range_scale = 150  # @param{type: 'number'}
 sat_scale = 0  # @param{type: 'number'}
-cutn_batches = 1  # @param{type: 'number'}
+cutn_batches = 2  # @param{type: 'number'}
 skip_augs = False  # @param{type: 'bodolean'}
 
 # @markdown ---
@@ -2448,7 +2448,7 @@ image_prompts = {
 # @title Do the Run!
 # @markdown `n_batches` ignored with animation modes.
 display_rate = 500  # @param{type: 'number'}
-n_batches = 2  # @param{type: 'number'}
+n_batches = 1  # @param{type: 'number'}
 
 # Update Model Settings
 timestep_respacing = f'ddim{steps}'
